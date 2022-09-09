6913dae038540ec0c937547871974b63cca5a610
taketakebamboo <takeuguisu@gmail.com>
2022-09-09 20:10:34 +0900

    詳細画面と削除処理、更新画面・処理の実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -17,7 +17,7 @@ class TweetController extends Controller
     {
         //一覧表示
         $tweets = Tweet::getAllOrderByUpdated_at();
-        ddd($tweets);
+        //ddd($tweets);
         return view('tweet.index',compact('tweets'));
     }
 
@@ -69,6 +69,8 @@ class TweetController extends Controller
     public function show($id)
     {
         //
+        $tweet = Tweet::find($id);
+        return view('tweet.show', compact('tweet'));
     }
 
     /**
@@ -80,6 +82,8 @@ class TweetController extends Controller
     public function edit($id)
     {
         //
+        $tweet = Tweet::find($id);
+        return view('tweet.edit', compact('tweet'));
     }
 
     /**
@@ -91,7 +95,21 @@ class TweetController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'tweet' => 'required | max:191',
+            'description' => 'required',
+        ]);
+        //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect()
+                ->route('tweet.edit', $id)
+                ->withInput()
+                ->withErrors($validator);
+        }
+        //データ更新処理
+        $result = Tweet::find($id)->update($request->all());
+        return redirect()->route('tweet.index');
     }
 
     /**
@@ -103,5 +121,7 @@ class TweetController extends Controller
     public function destroy($id)
     {
         //
+        $result = Tweet::find($id)->delete();
+        return redirect()->route('tweet.index');
     }
 }
